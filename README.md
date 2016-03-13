936ca1d5cf1592208d1f0f8ec8366f5dd0d0cea4
xumingjie1658 <flippyxu@gmail.com>
2016-03-14 07:57:09 +0800

    version 1.0.1

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # fekey-smarty
+基于百度外卖业务和fis3的针对 Smarty 模板的前端工程解决方案
---
+++ index.js
@@ -0,0 +1,153 @@
+
+/**
+ * fis.baidu.com
+ */
+
+var path = require('path');
+
+module.exports = function(fis, isMount) {
+  var sets = {
+    'namespace': '',
+    'static': 'static',
+    'template': 'template',
+    'smarty': {
+      'left_delimiter': '{%',
+      'right_delimiter': '%}'
+    }
+  };
+
+  fis.set('server.type', 'smarty');
+
+  var matchRules = {
+    // all release to $static dir 
+    '*': {
+      release: '/${static}/${namespace}/$0'
+    },
+    '*.{js,css,less}': {
+      useHash: true
+    },
+    '*.js': {
+      optimizer: fis.plugin('uglify-js')
+    },
+    '*.less': {
+      parser: fis.plugin('less'),
+      rExt: '.css'
+    },
+    '*.{css,less}': {
+      optimizer: fis.plugin('clean-css')
+    },
+    '::image': {
+      useHash: true
+    },
+    '*.png': {
+      optimizer: fis.plugin('png-compressor')
+    },
+    '/(**.tpl)': {
+      preprocessor: fis.plugin('extlang'),
+      optimizer: [
+        fis.plugin('smarty-xss'),
+        fis.plugin('html-compress')
+      ],
+      useMap: true,
+      release: '/${template}/${namespace}/$1'
+    },
+    '*.{tpl,js}': {
+      useSameNameRequire: true
+    },
+    // page dir
+    '/page/**.tpl': {
+      // 标记是否是个页面，向下兼容
+      extras: {
+        isPage: true
+      }
+    },
+    // widget
+    '/(widget/**.tpl)': {
+      url: '${namespace}/$1',
+      useMap: true,
+    },
+    '/widget/{*.{js,css},**/*.{js,css}}': {
+      isMod: true
+    },
+    '/{plugin/**.*,smarty.conf,domain.conf,**.php}': {
+      release: '$0'
+    },
+    'server.conf': {
+      release: '/server-conf/${namespace}.conf'
+    },
+    '/static/(**)': {
+      release: '/${static}/${namespace}/$1'
+    },
+    // test & config
+    '/(test)/(**)': {
+      useMap: false,
+      release: '/$1/${namespace}/$2'
+    },
+    '/(config)/(**)': {
+      useMap: false,
+      release: '/$1/${namespace}/$2'
+    },
+    '${namespace}-map.json': {
+      release: '/config/$0'
+    },
+    '*.sh': {
+      release: '$0'
+    },
+    '::package': {
+      prepackager: [
+        fis.plugin('widget-inline'),
+        fis.plugin('js-i18n')
+      ]
+    },
+    '*.{jsx,es6}' : {
+        rExt: '.js',
+        parser: fis.plugin('babel-5.x', {
+            blacklist: ['regenerator'],
+            stage: 3
+        }),
+        isMod: true,
+        useHash: true,
+        isJsLike: true
+    },
+    '{package.json, build.sh}' : {
+        release : false
+    }
+  };
+
+  function mount() {
+    // smarty
+    fis.set('system.localNPMFolder', path.join(__dirname, 'node_modules'));
+
+    fis.util.map(sets, function(key, value) {
+      fis.set(key, value);
+    });
+
+    fis.util.map(matchRules, function(selector, rules) {
+      fis.match(selector, rules);
+    });
+  
+    // 模块化支持
+    fis.hook('commonjs');
+
+    // map.json
+    fis.match('::package', {
+      postpackager: function createMap(ret) {
+        var path = require('path')
+        var root = fis.project.getProjectPath();
+        var map = fis.file.wrap(path.join(root, fis.get('namespace') ? fis.get('namespace') + '-map.json' : 'map.json'));;
+        map.setContent(JSON.stringify(ret.map, null, map.optimizer ? null : 4));
+        ret.pkg[map.subpath] = map;
+      }
+    });
+  }
+
+  if (isMount !== false) {
+    mount();
+  }
+
+  return {
+    loadPath: path.join(__dirname, 'node_modules'),
+    sets: sets,
+    matchRules: matchRules
+  }
+};
